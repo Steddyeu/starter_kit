d0fdc16fbbdac614ec31e558856b0d360a7c5d5e
Ed <edwardaacowell@gmail.com>
2021-07-26 19:57:05 +0100

    connected to metamask and connected to web3 in react

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if(window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert('Non-Ethereum browser detected. Please use Metamask')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account: accounts[0]})
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { account: ''};
+
+  }
+
   render() {
     return (
       <div>
@@ -13,8 +43,13 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
-          </a>
+          Colour Tokens
+            </a>
+            <ul>
+            <li>
+            Account Here
+            </li>
+            </ul>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
