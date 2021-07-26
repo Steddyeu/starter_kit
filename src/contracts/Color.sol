3f831ea3b7f7b174fad786e49fecc601b1c98b98
Ed <edwardaacowell@gmail.com>
2021-07-26 09:57:05 +0100

    created mint function along with tests

---
+++ src/contracts/Color.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.5.0;
+
+import "./ERC721Full.sol";
+
+contract Color is ERC721Full {
+  string[] public colors;
+
+  // mapping is similar to a has or json object, key/value pair
+  mapping(string => bool) _colorExists;
+
+  // Initialise function 
+  constructor () ERC721Full("Color", "COLOR") public {
+    
+  }
+
+  function mint(string memory _color) public {
+   uint _id = colors.push(_color);
+   _mint(msg.sender, _id);
+  _colorExists[_color] = true;
+  }
+
+}
